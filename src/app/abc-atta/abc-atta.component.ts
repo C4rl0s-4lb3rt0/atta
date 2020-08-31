e58f5355d73c4638a13b5ef4dc396d6b8a0e5a1a
EdwinEnriquezANZ <eenriquez@anzen.com.mx>
2020-08-31 11:10:30 -0500

    insertar nuevo usuario

--- src/app/abc-atta/abc-atta.component.ts
+++ src/app/abc-atta/abc-atta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import {AuthApiService} from "../services/auth-api.service";
 
 interface Business {
   value: string;
@@ -34,75 +35,81 @@ export class AbcAttaComponent implements OnInit {
   ];
 
 
-  levels: string[]= ['1','2','3','4'];
+  levels: string[] = ['1', '2', '3', '4'];
 
-  auxtext:Boolean=true;
+  auxtext: Boolean = true;
 
   constructor(private fb: FormBuilder,
-    private router: Router,
-    private _formBuilder: FormBuilder) {
+              private router: Router,
+              private _formBuilder: FormBuilder,
+              private api:AuthApiService
+  ) {
 
       this.crearFormulario();
-      this.language=localStorage.getItem('lenguaje')
-      console.log(this.language)
+      this.language = localStorage.getItem('lenguaje');
+      console.log(this.language);
       console.log(  this.isEnglish() );
-      console.log("vamos")
+      console.log('vamos');
     }
 
   ngOnInit(): void {
-  
-    console.log("ngonit")
-    if( !this.isEnglish() ){
+
+    console.log('ngonit');
+    if ( !this.isEnglish() ) {
       console.log('entra a español');
       this.firstFormGroup = this._formBuilder.group({
-        user:['', Validators.required],
-        firstName:['', Validators.required],
-        apPaterno:['', Validators.required],
-        apMaterno:['',Validators.required],
-        email:['',[Validators.required , Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-        phone:['',Validators.required],
-        business:['',Validators.required],
-        level:['',Validators.required], 
-        leng:['esp']
+        user: ['', Validators.required],
+        firstName: ['', Validators.required],
+        apPaterno: ['', Validators.required],
+        apMaterno: ['', Validators.required],
+        email: ['', [Validators.required , Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+        phone: ['', Validators.required],
+        business: ['', Validators.required],
+        level: ['', Validators.required],
+        leng: ['esp']
     });
-    }else{
+    } else {
       console.log('entra a ingle');
       this.firstFormGroup = this._formBuilder.group({
-          user:['', Validators.required],
-          firstName:['', Validators.required],
-          lastName:['',Validators.required],
-          email:['',[Validators.required , Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-          phone:['',Validators.required],
-          business:['',Validators.required],
-          level:['',Validators.required],
-          leng:['eng']
+          user: ['', Validators.required],
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          email: ['', [Validators.required , Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+          phone: ['', Validators.required],
+          business: ['', Validators.required],
+          level: ['', Validators.required],
+          leng: ['eng']
       });
     }
 
-   
+
 
   }
 
-  crearFormulario(){
+  crearFormulario() {
     // this.forma = this.fb.group({
-      
+
     // })
   }
-  isEnglish(){
-    let aux = localStorage.getItem('lenguaje').replace(/['"]+/g, '')
-    if(aux == 'en'){
-      return true
-    }else{
-      return false
+  isEnglish() {
+    const aux = localStorage.getItem('lenguaje').replace(/['"]+/g, '');
+    if (aux == 'en') {
+      return true;
+    } else {
+      return false;
     }
   }
 
-  guardar(){
-    if(this.forma.invalid){
-      this.auxtext= !this.auxtext;
-    }else{
-    
-      console.log(this.forma);
+  guardar() {
+    console.log('entro a guardar');
+    if (this.firstFormGroup.invalid) {
+      this.auxtext = !this.auxtext;
+    } else {
+
+      console.log(this.firstFormGroup);
+      this.api.insertUsers(this.firstFormGroup.value);
+
+
       // this.router.navigateByUrl('/dashboard');
     }
   }
