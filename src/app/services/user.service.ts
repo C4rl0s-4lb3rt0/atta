c36fff0260b9ec097497a51fdc325b21fb3ebf94
EdwinEnriquezANZ <eenriquez@anzen.com.mx>
2020-08-31 10:47:10 -0500

    getuser in user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 // import { User } from '../dashboard/dashboard.component';
-
+import { AuthApiService } from '../services/auth-api.service';
 @Injectable()
 
-export class UsersService{
+export class UsersService {
 
-     private user:User[] = [
-          { 
+  private varAux = this.apiRQ.getUsers().subscribe(resp => {
+    console.log('*******************');
+    console.log(resp);
+    this.user=resp;
+    console.log('*******************');
+  });
+     private user: User[] =  [
+          {
             id: '1',
-            name: 'Leanne Graham',
+            name: 'Leanne edo edo',
             username: 'aaBret',
             email: 'Sincere@april.biz',
             phone: '1-770-736-8031 x56442',
             website: 'hildegard.org',
             status: 'Active',
-            level:'2'
+            level: '2'
           },
           {
             id: '2',
@@ -24,7 +30,7 @@ export class UsersService{
             phone: '010-692-6593 x09125',
             website: 'anastasia.net',
             status: 'Blocked',
-            level:'1'
+            level: '1'
           },
           {
             id: '3',
@@ -34,7 +40,7 @@ export class UsersService{
             phone: '1-463-123-4447',
             website: 'ramiro.info',
             status: 'Blocked',
-            level:'3'
+            level: '3'
           },
           {
             id: '4',
@@ -44,7 +50,7 @@ export class UsersService{
             phone: '493-170-9623 x156',
             website: 'kale.biz',
             status: 'Active',
-            level:'1'
+            level: '1'
           },
           {
             id: '5',
@@ -54,52 +60,13 @@ export class UsersService{
             phone: '(254)954-1289',
             website: 'demarco.info',
             status: 'Active',
-            level:'3'
+            level: '3'
           },
-          {
-            id: '6',
-            name: 'Mrs. Dennis Schulist',
-            username: 'Leopoldo_Corkery',
-            email: 'Karley_Dach@jasper.info',
-            phone: '1-477-935-8478 x6430',
-            website: 'ola.org',
-            status: 'In-Active',
-            level:'1'
-          },
-          {
-            id: '7',
-            name: 'Kurtis Weissnat',
-            username: 'Elwyn Skiles',
-            email: 'Telly.Hoeger@billy.biz',
-            phone: '210.067.6132',
-            website: 'elvis.io',
-            status: 'Active',
-            level:'4'
-          },
-          {
-            id: '8',
-            name: 'Nicholas Runolfsdottir V',
-            username: 'Maxime_Nienow',
-            email: 'Sherwood@rosamond.me',
-            phone: '586.493.6943 x140',
-            website: 'jacynthe.com',
-            status: 'In-Active',
-            level:'1'
-          },
-          {
-            id: '10',
-            name: 'Clementina DuBuque',
-            username: 'Moriah.Stanton',
-            email: 'Rey.Padberg@karina.biz',
-            phone: '024-648-3804',
-            website: 'ambrose.net',
-            status: 'Active',
-            level:'2'
-          },
-     ]
 
-     private userBlacklist:User[] = [
-      { 
+     ];
+
+     private userBlacklist: User[] = [
+      {
         id: '423',
         name: 'Leanne Graham',
         username: 'Bret',
@@ -107,7 +74,7 @@ export class UsersService{
         phone: '1-770-736-8031 x56442',
         website: 'hildegard.org',
         status: 'Active',
-        level:'1'
+        level: '1'
       },
       {
         id: '223',
@@ -117,7 +84,7 @@ export class UsersService{
         phone: '010-692-6593 x09125',
         website: 'anastasia.net',
         status: 'Blocked',
-        level:'3'
+        level: '3'
       },
       {
         id: '312',
@@ -127,7 +94,7 @@ export class UsersService{
         phone: '1-463-123-4447',
         website: 'ramiro.info',
         status: 'Blocked',
-        level:'1'
+        level: '1'
       },
       {
         id: '544',
@@ -137,7 +104,7 @@ export class UsersService{
         phone: '493-170-9623 x156',
         website: 'kale.biz',
         status: 'Active',
-        level:'2'
+        level: '2'
       },
       {
         id: '695',
@@ -147,7 +114,7 @@ export class UsersService{
         phone: '(254)954-1289',
         website: 'demarco.info',
         status: 'Active',
-        level:'3'
+        level: '3'
       },
       {
         id: '876',
@@ -157,7 +124,7 @@ export class UsersService{
         phone: '1-477-935-8478 x6430',
         website: 'ola.org',
         status: 'In-Active',
-        level:'2'
+        level: '2'
       },
       {
         id: '547',
@@ -167,7 +134,7 @@ export class UsersService{
         phone: '210.067.6132',
         website: 'elvis.io',
         status: 'Active',
-        level:'1'
+        level: '1'
       },
       {
         id: '458',
@@ -177,9 +144,9 @@ export class UsersService{
         phone: '586.493.6943 x140',
         website: 'jacynthe.com',
         status: 'In-Active',
-        level:'3'
+        level: '3'
       },
-      
+
       {
         id: '453',
         name: 'Clementina DuBuque',
@@ -188,35 +155,61 @@ export class UsersService{
         phone: '024-648-3804',
         website: 'ambrose.net',
         status: 'Active',
-        level:'1'
+        level: '1'
       },
-      
- ]
-    //  private recluterId={'1232','3224','2354','2432','1276'}
-     private status=[
-       {name:'NO CONTACT'},{name:'EMAIL SENT'},{name:'FOLLOW UP'},{name:'WAITING RESPONSE'},{name:'NO RESPONSE'},{name:'SCREENING TO DO'},{name:'SCREENING PROCESS'},{name:'SCREENING DONE'},{name:'NO PROFILE'},{name:'PAYLOCITY'},{name:'WHATSAPP'}];
 
-     
+ ];
+    //  private recluterId={'1232','3224','2354','2432','1276'}
+     private status = [
+       {name: 'NO CONTACT'}, {name: 'EMAIL SENT'}, {name: 'FOLLOW UP'}, {name: 'WAITING RESPONSE'}, {name: 'NO RESPONSE'}, {name: 'SCREENING TO DO'}, {name: 'SCREENING PROCESS'}, {name: 'SCREENING DONE'}, {name: 'NO PROFILE'}, {name: 'PAYLOCITY'}, {name: 'WHATSAPP'}];
+
+
     //  private status:string[]=["Saab", "Volvo", "BMW"];
     //  private contact=[{'EMAIL','WHATSAPP','FOLLOW UP EMAIL'}]
     //  private blacklist=[{'MOVE TO BLACKLIST','MOVE TO JOB APPLICANTS'}]
+  private api: AuthApiService;
 
-     constructor(){
+     constructor(private apiRQ:AuthApiService) {
+       this.api=apiRQ
           console.log('servico listo para usarse');
+          console.log('con el auth service creado');
+          const varAux = this.apiRQ.getUsers().subscribe(resp => {
+            console.log('*******************');
+            console.log(resp);
+            this.user=resp;
+            console.log('*******************');
+          });
+       this.getUsers();
+
+
+
+
+
           this.getUser('1');
+
      }
 
-     getUsers(){
-          return this.user;
+
+     getUsers() {
+
+       const varAux = this.apiRQ.getUsers().subscribe(resp => {
+         console.log('*******************');
+         console.log(resp);
+         this.user=resp;
+         console.log('*******************');
+       });
+
+
+       return this.user;
      }
-     getBlacklist(){
+     getBlacklist() {
           return this.userBlacklist;
      }
-     getUser(idUser:string){
-          let aux = this.user.find(x => x.id === idUser);
+     getUser(idUser: string) {
+          const aux = this.user.find(x => x.id === idUser);
           return aux;
      }
-     getStatus(){
+     getStatus() {
        return this.status;
      }
     //  getRecluterId(){
@@ -233,12 +226,12 @@ export class UsersService{
 }
 
 export interface User {
-     id: string,
-     name: string,
-     username: string,
-     email: string,
-     phone: string,
-     website: string,
-     status: string,
-     level?: string;
+     id: string;
+     name: string;
+     username: string;
+     email: string;
+     phone: string;
+     website: string;
+     status: string;
+     level: string;
    }
